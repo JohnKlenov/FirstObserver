85b68881d72f1f249ace63bd0870aa755ee755b3
JohnKlenov <klenovptz@mail.ru>
2022-11-09 10:38:38 +0300

    Create UIScrollView on the MallViewController

---
+++ FirstObserver/Controllers/MallViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MallViewController.swift
+//  FirstObserver
+//
+//  Created by Evgenyi on 7.11.22.
+//
+
+import UIKit
+
+class MallViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let guide = self.view.safeAreaLayoutGuide
+        let heightSafeArea = guide.layoutFrame.height
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
